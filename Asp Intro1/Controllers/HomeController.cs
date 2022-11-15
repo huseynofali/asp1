1369b774923810dda9c458d99aca0b5866210bfb
huseynofali <huseynof.alieh@code.edu.az>
2022-11-16 03:28:45 +0400

    homework.md file

---
+++ Asp Intro1/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+namespace Asp_Intro1.Controllers
+{
+       public class HomeController : Controller
+       {
+            public ViewResult Index()
+            {
+                ViewResult vr = new ViewResult();
+                vr.ViewName = "index";
+                return vr;
+            }
+
+            public ViewResult About()
+            {
+                ViewResult vr = new ViewResult();
+                vr.ViewName = "about";
+                return vr;
+            }
+
+            public ViewResult Contact()
+            {
+                ViewResult vr = new ViewResult();
+                vr.ViewName = "contact";
+                return vr;
+            }
+
+       }
+    
+}
